9f9bf72c94af376700b38936c423a30afbd361e9
Kirbee Parsons <kirbeeleanne@gmail.com>
2019-04-27 20:02:16 -0700

    add styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
         icon: `src/images/k.png` // This path is relative to the root of the site.
       }
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
