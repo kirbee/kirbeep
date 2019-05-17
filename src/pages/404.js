45fd4bbb8bc9934a1453cd11974e2efbd68bacdb
Kirbee Parsons <kirbeeleanne@gmail.com>
2019-05-16 21:49:17 -0700

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { H1 } from 'common/H1';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
+    <H1>Not found</H1>
     <p>Oops ... there's nothing here ...</p>
   </Layout>
 );
